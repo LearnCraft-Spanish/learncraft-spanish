a28664ca0855a9b5c1e5e56ce42d1cb6172044fa
ClassicalMoser <69487098+ClassicalMoser@users.noreply.github.com>
2022-10-14 12:12:16 -0500

    Updates

---
+++ precache-manifest.1f40e368954386e969f0304906b8dbca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8160e7ce4d130b194b4d",
+    "url": "/learncraft-spanish/static/css/main.4ffd085f.chunk.css"
+  },
+  {
+    "revision": "8160e7ce4d130b194b4d",
+    "url": "/learncraft-spanish/static/js/main.8160e7ce.chunk.js"
+  },
+  {
+    "revision": "836e4380fe3f7816a983",
+    "url": "/learncraft-spanish/static/js/1.836e4380.chunk.js"
+  },
+  {
+    "revision": "191edb2c693071f4d929",
+    "url": "/learncraft-spanish/static/js/2.191edb2c.chunk.js"
+  },
+  {
+    "revision": "3607b69519a9427500c8",
+    "url": "/learncraft-spanish/static/js/runtime~main.3607b695.js"
+  },
+  {
+    "revision": "bfbcb3138f884f31cd74da017149491a",
+    "url": "/learncraft-spanish/index.html"
+  }
+];
