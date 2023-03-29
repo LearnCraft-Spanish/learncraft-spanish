bcffad7e679750e3a5d1e57db6152364baec9a14
ClassicalMoser <69487098+ClassicalMoser@users.noreply.github.com>
2023-03-29 14:04:30 -0500

    run build on basic auth0

---
+++ build/precache-manifest.5312c1697d7254d0395ac26342362ad3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6a58f2d4c5df7f91996",
+    "url": "/static/css/main.e97e16a7.chunk.css"
+  },
+  {
+    "revision": "d6a58f2d4c5df7f91996",
+    "url": "/static/js/main.d6a58f2d.chunk.js"
+  },
+  {
+    "revision": "2d3efeb71c7eebfe7cd0",
+    "url": "/static/js/1.2d3efeb7.chunk.js"
+  },
+  {
+    "revision": "637e5880d959743857a3",
+    "url": "/static/js/2.637e5880.chunk.js"
+  },
+  {
+    "revision": "e279bb2fcb32ee98b7c8",
+    "url": "/static/js/runtime~main.e279bb2f.js"
+  },
+  {
+    "revision": "409c2d852a00b4962d571027622c6f6e",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2e3a65dd35d1fce3a3f300f68ab0c837.js"
+  "/precache-manifest.5312c1697d7254d0395ac26342362ad3.js"
 );
 
 workbox.clientsClaim();
