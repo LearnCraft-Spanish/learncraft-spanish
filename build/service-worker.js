0fa7d80a163f923c890f21427877fb9bdd6e78ec
ClassicalMoser <69487098+ClassicalMoser@users.noreply.github.com>
2023-04-03 18:40:49 -0500

    Design Upgrades

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e1818ce667feb7f6f59ac262ed82b5d2.js"
+  "/precache-manifest.07eacf1af871650035dbe64aa001506a.js"
 );
 
 workbox.clientsClaim();
