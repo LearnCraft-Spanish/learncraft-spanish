f56dabc4064dc8ae1ce11077bcff46fd3cd8ab9b
ClassicalMoser <69487098+ClassicalMoser@users.noreply.github.com>
2024-05-24 13:57:45 -0500

    add sum test

---
+++ tests/sum.test.js
@@ -0,0 +1,11 @@
+// sum.test.js
+import { expect, test } from 'vitest'
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3)
+})
+
+export function sum(a, b) {
+    return a + b
+  }
+
