96468bd69690af08139658c439960e543c0f724c
ClassicalMoser <69487098+ClassicalMoser@users.noreply.github.com>
2024-05-24 14:00:00 -0500

    add test for test

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -5,6 +5,10 @@ test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3)
 })
 
+test('can test', () => {
+  expect (test).toBeDefined()
+})
+
 export function sum(a, b) {
     return a + b
   }
