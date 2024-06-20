117d8dc44601315efb22d3e041b8651a65ed475c
ClassicalMoser <69487098+ClassicalMoser@users.noreply.github.com>
2024-06-19 19:00:20 -0500

    remove vite sentry import typo

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import { sentryVitePlugin } from '@sentry/vite-plugin';
 
 export default defineConfig(({ mode }) => {
   // Load environment variables based on the mode
