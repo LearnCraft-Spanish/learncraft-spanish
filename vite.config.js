77bf5569c37a0c11582fd3aa337bcac7431f0420
ClassicalMoser <69487098+ClassicalMoser@users.noreply.github.com>
2024-06-19 20:11:36 -0500

    configure sentry for vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import dotenv from 'dotenv';
+dotenv.config();
 
 export default defineConfig(({ mode }) => {
   // Load environment variables based on the mode
@@ -12,6 +15,11 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [
         react(),
+        sentryVitePlugin({
+            authToken: process.env.SENTRY_AUTH_TOKEN,
+            org: "learncraft-spanish",
+            project: "learncraft-spanish-frontend",
+            })
     ],
     build: {
       outDir: 'build',
