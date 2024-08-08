73a6187c2f526e1be7d2fe842f9471e2d4258a7d
VirtualBlakeWillis <bewillis101@gmail.com>
2024-08-08 15:14:58 -0500

    LessonSelector converted to TypeScript, began defining interfaces in interfaceDefinitions

---
+++ src/LessonSelector.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+
+
+export default function LessonSelector(
+  programTable: Array<program>,
+  selectedLesson: lesson | null,
+  updateSelectedLesson: Function,
+  selectedProgram: program,
+  updateSelectedProgram: Function,
+) {
+  function makeCourseSelector() {
+    const courseSelector = [
+      <option key={0} value={0}>
+        –Choose Course–
+      </option>,
+    ];
+    programTable.forEach((item: program) => {
+      courseSelector.push(
+        <option key={item.recordId} value={item.recordId}>
+          {' '}
+          {item.name}
+        </option>,
+      );
+    });
+    return courseSelector;
+  }
+
+  function makeLessonSelector() {
+    const lessonSelector: Array<JSX.Element> = [];
+    selectedProgram.lessons.forEach((lesson: lesson) => {
+      const lessonArray = lesson.lesson.split(' ');
+      const lessonNumber = lessonArray.slice(-1)[0];
+      lessonSelector.push(
+        <option key={lesson.lesson} value={lesson.recordId}>
+          {' '}
+          Lesson {lessonNumber}
+        </option>,
+      );
+    });
+    return lessonSelector;
+  }
+
+  return (
+    <div>
+      <div className="lessonFilter">
+        <form onSubmit={(e) => e.preventDefault}>
+          <h3>Set Level</h3>
+          <select
+            className="courseList"
+            value={selectedProgram.recordId}
+            onChange={(e) => updateSelectedProgram(e.target.value)}
+          >
+            {makeCourseSelector()}
+          </select>
+          {selectedLesson && selectedProgram.lessons && (
+            <select
+              className="lessonList"
+              value={selectedLesson.recordId}
+              onChange={(e) => updateSelectedLesson(e.target.value)}
+            >
+              {makeLessonSelector()}
+            </select>
+          )}
+        </form>
+      </div>
+    </div>
+  );
+}
---
+++ src/interfaceDefinitions.tsx
@@ -0,0 +1,19 @@
+interface lesson {
+    recordId: number;
+    lesson: string;
+    vocabIncluded: Array<string>;
+    sortReference: number;
+    relatedProgram: number;
+    vocabKnown: Array<string>;
+  }
+  
+  interface program {
+    recordId: number;
+    name: string;
+    lessons: Array<lesson>;
+    cohortACurrentLesson: number;
+    cohortBCurrentLesson: number;
+    cohortCCurrentLesson: number;
+    cohortDCurrentLesson: number;
+    cohortECurrentLesson: number;
+  }
