b509edcdf15d3d99fbf30a867f808055ab554fb4
VirtualBlakeWillis <bewillis101@gmail.com>
2024-08-09 11:33:59 -0500

    fixed process import

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
+import process from 'node:process'
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { sentryVitePlugin } from '@sentry/vite-plugin'
 import dotenv from 'dotenv'
 
 dotenv.config()
-
-const process = require('node:process')
 
 export default defineConfig(({ mode }) => {
   // Load environment variables based on the mode
