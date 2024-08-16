760a373c06cfb8a4a0735baa8a7aa93e85dc623c
VirtualBlakeWillis <bewillis101@gmail.com>
2024-08-16 11:10:47 -0500

    continuing Quiz Component development

--- src/Quiz.tsx
+++ src/Quiz.tsx
@@ -6,14 +6,33 @@ import QuizButtons from './components/QuizButtons'
 import QuizProgress from './components/QuizProgress'
 import MenuButton from './components/MenuButton'
 
+// removeFlashcardAndUpdate
+// addFlashcardAndUpdate
 export default function Quiz({
   dataLoaded,
   quizReady,
   makeQuizTitle,
+  examplesToReview,
+  activeStudent,
+  addFlashcard,
+  removeFlashcard,
+  makeMenuShow,
+  
 
 }) {
   const [answerShowing, setAnswerShowing] = useState(false)
+  const [currentExample, setCurrentExample] = useState(examplesToReview[0])
+  const [currentExampleNumber, setCurrentExampleNumber] = useState(1)
+  const currentAudio = useRef(null)
+  const [playing, setPlaying] = useState(false)
 
+  function toggleAnswer() {
+    if (currentAudio.current) {
+      currentAudio.current.currentTime = 0
+    }
+    setPlaying(false)
+    setAnswerShowing(!answerShowing)
+  }
   /*      Audio Component Section       */
 
   const spanishAudioUrl = currentExample?.spanishAudioLa
@@ -57,6 +76,56 @@ export default function Quiz({
     setPlaying(false)
     currentAudio.current.pause()
   }, [currentAudio])
+
+  const togglePlaying = useCallback(() => {
+    playing ? pauseCurrentAudio() : playCurrentAudio()
+  }, [playing, pauseCurrentAudio, playCurrentAudio])
+
+  /*     Add/Remove Flashcard Section       */
+  function incrementExample() {
+    if (currentExampleNumber < examplesToReview.length) {
+      const newExampleNumber = currentExampleNumber + 1
+      setCurrentExampleNumber(newExampleNumber)
+    }
+    else {
+      setCurrentExampleNumber(examplesToReview.length)
+    }
+    hideAnswer()
+    setPlaying(false)
+  }
+
+  function decrementExample() {
+    if (currentExampleNumber > 1) {
+      setCurrentExampleNumber(currentExampleNumber - 1)
+    }
+    else {
+      setCurrentExampleNumber(1)
+    }
+    hideAnswer()
+    setPlaying(false)
+  }
+  async function removeFlashcardAndUpdate(recordId) {
+    const currentExample = examplesToReview.find(
+      example => example.recordId === recordId,
+    )
+    decrementExample()
+    currentExample.isKnown = false
+    const flashcardRemovedPromise = removeFlashcard(recordId)
+    if (await flashcardRemovedPromise !== 1) {
+      currentExample.isKnown = true
+    }
+  }
+  async function addFlashcardAndUpdate(recordId) {
+    const currentExample = examplesToReview.find(
+      example => example.recordId === recordId,
+    )
+    incrementExample()
+    currentExample.isKnown = true
+    const flashcardAddedPromise = addFlashcard(recordId)
+    if (await flashcardAddedPromise !== 1) {
+      currentExample.isKnown = false
+    }
+  }
 
   return (
     <div>
