5cd50f93c95157262d92f52c66924eaf0c0b7557
ClassicalMoser <69487098+ClassicalMoser@users.noreply.github.com>
2024-08-21 15:43:07 -0500

    debounce install and implement for table update

--- src/contexts/ActiveStudentContext.tsx
+++ src/contexts/ActiveStudentContext.tsx
@@ -1,5 +1,6 @@
 import React, { createContext, useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import type { ReactNode } from 'react'
+import debounce from 'lodash/debounce'
 import type { Flashcard, StudentExample, StudentFlashcardData, UserData } from '../interfaceDefinitions' // Adjust the import based on your project structure
 import { useBackend } from '../hooks/useBackend'
 import { useUserData } from '../hooks/useUserData'
@@ -8,13 +9,13 @@ interface ActiveStudentContextProps {
   activeStudent: UserData | null
   setActiveStudent: (student: UserData | null) => void
   studentFlashcardData: StudentFlashcardData | null
-  setStudentFlashcardData: (examples: StudentExample[]) => void
   choosingStudent: boolean
   chooseStudent: () => void
   keepStudent: () => void
   studentList: UserData[]
   updateActiveStudent: (studentID: number) => void
   flashcardDataSynced: boolean
+  syncFlashcards: () => void
 }
 
 const ActiveStudentContext = createContext<ActiveStudentContextProps | undefined>(undefined)
@@ -26,14 +27,22 @@ interface ActiveStudentProviderProps {
 export function ActiveStudentProvider({ children }: ActiveStudentProviderProps) {
   const { userData } = useUserData()
   const { getAllUsersFromBackend, getActiveExamplesFromBackend, getMyExamplesFromBackend } = useBackend()
+
+  // UserData object of the active student
   const [activeStudent, setActiveStudent] = useState<UserData | null>(null)
+
+  // If admin, can change the active student from list
   const [studentList, setStudentList] = useState<UserData[]>([])
   const [choosingStudent, setChoosingStudent] = useState(false)
 
+  // Flashcard data, recently restructured as an object with two arrays as traits
   const [studentFlashcardData, setStudentFlashcardData] = useState<StudentFlashcardData | null>(null)
+
+  // Whether the state is up to date with the actual database
   const [flashcardDataSynced, setFlashcardDataSynced] = useState(false)
+
+  // Increments to reject stale data
   const syncNumber = useRef(0)
-  const latestSync = useRef <Promise <boolean> | null>(null)
 
   const setupStudentList = useCallback(async () => {
     getAllUsersFromBackend()
@@ -81,8 +90,8 @@ export function ActiveStudentProvider({ children }: ActiveStudentProviderProps) 
   }, [activeStudent, userData?.isAdmin, getActiveExamplesFromBackend, getMyExamplesFromBackend])
 
   const value = useMemo(
-    () => ({ activeStudent, setActiveStudent, studentFlashcardData, setStudentFlashcardData, choosingStudent, chooseStudent, keepStudent, studentList, updateActiveStudent, flashcardDataSynced }),
-    [activeStudent, studentFlashcardData, choosingStudent, studentList, updateActiveStudent],
+    () => ({ activeStudent, setActiveStudent, studentFlashcardData, choosingStudent, chooseStudent, keepStudent, studentList, updateActiveStudent, flashcardDataSynced, syncFlashcards }),
+    [activeStudent, studentFlashcardData, choosingStudent, studentList, updateActiveStudent, flashcardDataSynced, syncFlashcards],
   )
 
   useEffect(() => {
@@ -94,8 +103,9 @@ export function ActiveStudentProvider({ children }: ActiveStudentProviderProps) 
   useEffect(() => {
     if (activeStudent?.recordId) {
       setFlashcardDataSynced(false)
+      debounce(syncFlashcards, 500)
     }
-  }, [studentFlashcardData?.examples, studentFlashcardData?.studentExamples, activeStudent])
+  }, [studentFlashcardData?.examples, studentFlashcardData?.studentExamples, activeStudent, syncFlashcards])
 
   return (
     <ActiveStudentContext.Provider value={value}>
