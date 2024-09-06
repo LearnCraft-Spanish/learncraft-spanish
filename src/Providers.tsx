5f3d19c20fb9587d570b6af1ed347b27686ecec2
ClassicalMoser <69487098+ClassicalMoser@users.noreply.github.com>
2024-09-06 14:31:45 -0500

    Initialize React Query

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -3,6 +3,8 @@ import type { ReactNode } from 'react'
 import React from 'react'
 import { Auth0Provider } from '@auth0/auth0-react'
 import { useNavigate } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { ActiveStudentProvider } from './contexts/ActiveStudentContext'
 import { UserDataProvider } from './contexts/UserDataContext'
 
@@ -17,6 +19,7 @@ function Providers({ children }: ProvidersProps) {
   const redirect_uri = import.meta.env.VITE_LOCAL_DOMAIN
 
   const navigate = useNavigate()
+  const queryClient = new QueryClient()
 
   return (
     <Auth0Provider
@@ -32,11 +35,14 @@ function Providers({ children }: ProvidersProps) {
         navigate(appState?.targetUrl || '/', { replace: true })
       }}
     >
-      <UserDataProvider>
-        <ActiveStudentProvider>
-          {children}
-        </ActiveStudentProvider>
-      </UserDataProvider>
+      <QueryClientProvider client={queryClient}>
+        <UserDataProvider>
+          <ActiveStudentProvider>
+            {children}
+          </ActiveStudentProvider>
+        </UserDataProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </Auth0Provider>
   )
 }
