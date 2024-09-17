8e6c89d4279b1e9195c00758e42df5cffb0486d3
ClassicalMoser <69487098+ClassicalMoser@users.noreply.github.com>
2024-09-17 16:46:15 -0500

    Convert MockLessonData to JSON

--- tests/mockData.ts
+++ tests/mockData.ts
@@ -1,13 +1,14 @@
 import type { Lesson, Program, StudentFlashcardData, UserData } from '../src/interfaceDefinitions'
 import mockDbData from './mockDbData.json'
-import { lessonData } from './mockLessonData'
+import mockLessonData from './mockLessonData.json'
 
 export const sampleStudent: UserData = mockDbData.sampleStudent
 
 export const sampleStudentFlashcardData: StudentFlashcardData = mockDbData.sampleStudentFlashcardData
 
 export const samplePrograms = mockDbData.samplePrograms as Program[]
-const lessonDataTyped = lessonData as Lesson[]
+
+export const lessonDataTyped = mockLessonData.lessonData as Lesson[]
 
 samplePrograms.forEach((program: Program) => {
   program.lessons = lessonDataTyped.filter((lesson: Lesson) => lesson.relatedProgram === program.recordId)
