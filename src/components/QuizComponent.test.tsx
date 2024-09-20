ad86bc5afcf898dbe640f58263154c7e0a4aa4d7
VirtualBlakeWillis <bewillis101@gmail.com>
2024-09-20 11:50:35 -0500

    Quiz Component, removed tests that are covered by other test suites (ex: srsbutton testing)

--- src/components/QuizComponent.test.tsx
+++ src/components/QuizComponent.test.tsx
@@ -16,6 +16,15 @@ const cleanupFunction = vi.fn()
 
 vi.mock('../contexts/UserDataContext')
 vi.mock('../contexts/ActiveStudentContext')
+
+// vi.mock('../hooks/useStudentFlashcards', () => {
+//   return {
+//     useStudentFlashcards: () => ({
+//       flashcardDataQuery: { data: sampleStudentFlashcardData },
+//       exampleIsCollected: { data: vi.fn(() => false) },
+//     }),
+//   }
+// })
 /*
   quizTitle,
   examplesToParse,
@@ -80,27 +89,12 @@ describe('component Quiz', () => {
     })
     expect(cleanupFunction).toHaveBeenCalledOnce()
   })
-  it('updates flashcard on click', () => {
-    renderQuizNoSrs()
-    // Find the flashcard element
-    const flashcard = screen.getByRole('button', { name: 'flashcard' })
-    // Get the initial text content of the flashcard
-    const initialText = flashcard.textContent
 
-    // Simulate a click on the flashcard
-    act(() => {
-      fireEvent.click(flashcard)
-    })
-    const flashcard2 = screen.getByRole('button', { name: 'flashcard' })
-
-    // Assert that the flashcard text content has changed
-    expect(flashcard2.textContent).not.toBe(initialText)
-  })
   it('changes flashcard on next button click', () => {
     renderQuizNoSrs()
     // Find the flashcard element
-    const flashcard = screen.getByRole('button', { name: 'flashcard' })
-    // Get the initial text content of the flashcard
+    // Get the initial text content of the flashcard 
+    const flashcard = screen.getByLabelText('flashcard')
     const initialText = flashcard.textContent
 
     // Simulate a click on the flashcard
@@ -117,7 +111,7 @@ describe('component Quiz', () => {
     })
 
     // Find the flashcard element again
-    const flashcard2 = screen.getByRole('button', { name: 'flashcard' })
+    const flashcard2 = screen.getByLabelText('flashcard')
 
     // Assert that the flashcard text content has changed
     expect(flashcard2.textContent).not.toBe(initialText)
@@ -125,7 +119,7 @@ describe('component Quiz', () => {
   it('changes flashcard to previous on previous button click', () => {
     renderQuizNoSrs()
     // Find the flashcard element
-    const flashcard = screen.getByRole('button', { name: 'flashcard' })
+    const flashcard = screen.getByLabelText('flashcard')
     // Get the initial text content of the flashcard
     const initialText = flashcard.textContent
 
@@ -141,7 +135,7 @@ describe('component Quiz', () => {
     act(() => {
       fireEvent.click(nextButton)
     })
-    const flashcardNext = screen.getByRole('button', { name: 'flashcard' })
+    const flashcardNext = screen.getByLabelText('flashcard')
     const nextTest = flashcardNext.textContent
     // Find the previous button
     const previousButton = screen.getByRole('button', { name: 'Previous' })
@@ -152,19 +146,17 @@ describe('component Quiz', () => {
     })
 
     // Find the flashcard element again
-    const flashcardPrevious = screen.getByRole('button', { name: 'flashcard' })
+    const flashcardPrevious = screen.getByLabelText('flashcard')
 
     // Assert that the flashcard text content has changed
     expect(flashcardPrevious.textContent).toBe(initialText)
     expect(flashcardPrevious.textContent).not.toBe(nextTest)
   })
 
-  // These cannot be tested until userDataContext is finished
-  // may even be moved to SRSButtons file
   describe('isSrsQuiz is true', () => {
     it('renders SrsButtons when isSrsQuiz is true', () => {
       renderQuizYesSrs()
-      const flashcard = screen.getByRole('button', { name: 'flashcard' })
+      const flashcard = screen.getByLabelText('flashcard')
       act(() => {
         fireEvent.click(flashcard)
       })
@@ -174,134 +166,5 @@ describe('component Quiz', () => {
       expect(hardBtn).toBeTruthy()
       expect(easyBtn).toBeTruthy()
     })
-    it('renders Labeled: Hard when difficulty hard is selected', () => {
-      renderQuizYesSrs()
-      const flashcard = screen.getByRole('button', { name: 'flashcard' })
-      act(() => {
-        fireEvent.click(flashcard)
-      })
-      const hardBtn = screen.getByRole('button', { name: 'This was hard' })
-      act(() => {
-        fireEvent.click(hardBtn)
-      })
-      const previousBtn = screen.getByRole('button', { name: 'Previous' })
-      act(() => {
-        fireEvent.click(previousBtn)
-      })
-      const hardBanner = screen.getByRole('button', { name: 'Labeled: Hard' })
-      expect(hardBanner).toBeTruthy()
-    })
-    it('renders Labeled: Easy when difficulty easy is selected', () => {
-      renderQuizYesSrs()
-      const flashcard = screen.getByRole('button', { name: 'flashcard' })
-      act(() => {
-        fireEvent.click(flashcard)
-      })
-      const easyBtn = screen.getByRole('button', { name: 'This was easy' })
-      act(() => {
-        fireEvent.click(easyBtn)
-      })
-      const previousBtn = screen.getByRole('button', { name: 'Previous' })
-      act(() => {
-        fireEvent.click(previousBtn)
-      })
-      const easyBanner = screen.getByRole('button', { name: 'Labeled: Easy' })
-      expect(easyBanner).toBeTruthy()
-    })
-  })
-
-  it('updates QuizProgress on next button click', () => {
-    renderQuizNoSrs()
-    const flashcard = screen.getByRole('button', { name: 'flashcard' })
-    act(() => {
-      fireEvent.click(flashcard)
-    })
-    const nextButton = screen.getByRole('button', { name: 'Next' })
-    act(() => {
-      fireEvent.click(nextButton)
-    })
-    const totalExamplesNumber = sampleStudentFlashcardData.examples.length
-    const quizProgress = screen.getByText(`Flashcard 2 of ${totalExamplesNumber}`)
-    expect(quizProgress).toBeTruthy()
-  })
-  // cannot be fixed until userDataContext is finished
-  it.skip('removeFlashcard is called on remove button click', () => {
-    renderQuizNoSrs()
-    const flashcard = screen.getByRole('button', { name: 'flashcard' })
-    act(() => {
-      fireEvent.click(flashcard)
-    })
-    const removeButton = screen.getByRole('button', { name: 'Remove from my flashcards' })
-    act(() => {
-      fireEvent.click(removeButton)
-    })
-    expect(removeFlashcard).toHaveBeenCalled()
-  })
-  // cannot be fixed until userDataContext is finished
-  it.skip('addFlashcard is called on add button click', () => {
-    const unknownExample = {
-      recordId: 1,
-      spanglish: 'esp',
-      spanishExample: 'Hola',
-      englishTranslation: 'Hello',
-      isCollected: false,
-      vocabIncluded: ['hello'],
-      englishAudio: '',
-      spanishAudioLa: '',
-      vocabComplete: false,
-    }
-    render(
-      <MemoryRouter>
-        <QueryClientProvider client={queryClient}>
-          <Quiz
-            quizTitle="Test Quiz"
-            examplesToParse={[unknownExample]}
-            // startWithSpanish={false}
-            // quizOnlyCollectedExamples={false}
-            // isSrsQuiz={false}
-            // quizLength={1000}
-            cleanupFunction={cleanupFunction}
-          />
-        </QueryClientProvider>
-      </MemoryRouter>,
-    )
-    const flashcard = screen.getByRole('button', { name: 'flashcard' })
-    act(() => {
-      fireEvent.click(flashcard)
-    })
-    const addButton = screen.getByRole('button', { name: 'Add to my flashcards' })
-    act(() => {
-      fireEvent.click(addButton)
-    })
-    expect(addFlashcard).toHaveBeenCalled()
-  })
-
-  // Skipped, throwing error and needs to be updated
-  it('calls togglePlaying on audio button click', () => {
-    render(
-      <MemoryRouter>
-        <QueryClientProvider client={queryClient}>
-          <Quiz
-            quizTitle="Test Quiz"
-            examplesToParse={[sampleStudentFlashcardData.examples[2]]}
-            // startWithSpanish={false}
-            // quizOnlyCollectedExamples={false}
-            // isSrsQuiz={false}
-            // userData={sampleStudent}
-            // quizLength={1000}
-            cleanupFunction={cleanupFunction}
-          />
-        </QueryClientProvider>
-      </MemoryRouter>,
-    )
-    const flashcard = screen.getByRole('button', { name: 'flashcard' })
-    act(() => {
-      fireEvent.click(flashcard)
-    })
-    const audioButton = screen.getByRole('button', { name: 'Play Audio' })
-    act(() => {
-      fireEvent.click(audioButton)
-    })
-    expect(audioButton).toBeTruthy()
   })
 })
