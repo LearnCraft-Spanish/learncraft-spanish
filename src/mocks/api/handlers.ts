3d130f86020b46dc2fa39c9fccc8bfc6b7c65609
ClassicalMoser <69487098+ClassicalMoser@users.noreply.github.com>
2024-09-27 11:44:09 -0500

    Handlers for new data routes

--- src/mocks/api/handlers.ts
+++ src/mocks/api/handlers.ts
@@ -2,20 +2,38 @@ import { HttpResponse, http } from 'msw'
 import data from '../data/mockBackendData.json'
 
 const api = data.api
-const runningMode = import.meta.env.MODE
 const backendUrl = import.meta.env.VITE_BACKEND_URL
 
-console.log('mode:', runningMode)
-console.log('backendUrl:', backendUrl)
-
 export const handlers = [
+  http.get(`${backendUrl}public/my-data`, () => {
+    return HttpResponse.json(api.myData)
+  }),
+
+  http.get(`${backendUrl}public/all-students`, () => {
+    return HttpResponse.json(api.allStudentsTable)
+  }),
+
   http.get(`${backendUrl}public/programs`, () => {
-    console.log('hits the programs table!')
     return HttpResponse.json(api.programsTable)
   }),
 
   http.get(`${backendUrl}public/lessons`, () => {
-    console.log('hits the lessons table!')
     return HttpResponse.json(api.lessonsTable)
+  }),
+
+  http.get(`${backendUrl}public/vocabulary`, () => {
+    return HttpResponse.json(api.vocabularyTable)
+  }),
+
+  http.get(`${backendUrl}public/spellings`, () => {
+    return HttpResponse.json(api.spellingsTable)
+  }),
+
+  http.get(`${backendUrl}public/verified-examples`, () => {
+    return HttpResponse.json(api.verifiedExamplesTable)
+  }),
+
+  http.get(`${backendUrl}public/quizzes`, () => {
+    return HttpResponse.json(api.quizzesTable)
   }),
 ]
