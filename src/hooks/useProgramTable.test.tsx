166d519411a7b0dfe07e6bf3b5ed4149f628b2ed
VirtualBlakeWillis <bewillis101@gmail.com>
2024-10-03 16:07:34 -0500

    updated testing, useProgramTable tested

--- src/hooks/useProgramTable.test.tsx
+++ src/hooks/useProgramTable.test.tsx
@@ -1,11 +1,9 @@
 import { renderHook, waitFor } from '@testing-library/react'
-import { beforeAll, describe, expect, it, vi } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import data from '../mocks/data/serverlike/mockBackendData.json'
 
 import { useProgramTable } from './useProgramTable'
-
-// Override the default project-wide mock
 
 const api = data.api
 
@@ -13,22 +11,21 @@ interface WrapperProps {
   children: React.ReactNode
 }
 
+vi.unmock('@auth0/auth0-react')
+vi.mock('@auth0/auth0-react', () => ({
+  useAuth0: vi.fn(() => ({
+    isAuthenticated: true,
+  })),
+}))
+vi.mock('../hooks/useBackend', () => ({
+  useBackend: vi.fn(() => ({
+    getLessonsFromBackend: vi.fn(() => api.lessonsTable),
+    getProgramsFromBackend: vi.fn(() => api.programsTable),
+  })),
+}))
+
 describe('useProgramTable', () => {
-  beforeAll(() => {
-    vi.unmock('@auth0/auth0-react')
-    vi.mock('@auth0/auth0-react', () => ({
-      useAuth0: vi.fn(() => ({
-        isAuthenticated: true,
-      })),
-    }))
-    vi.mock('../hooks/useBackend', () => ({
-      useBackend: vi.fn(() => ({
-        getLessonsFromBackend: vi.fn(() => api.programsTable),
-        getProgramsFromBackend: vi.fn(() => api.lessonsTable),
-      })),
-    }))
-  })
-  it('runs without crashing', async () => {
+  it('renders with correct mocks', async () => {
     const queryClient = new QueryClient()
     const wrapper = ({ children }: WrapperProps) => (
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
@@ -36,6 +33,27 @@ describe('useProgramTable', () => {
     const { result } = renderHook(() => useProgramTable(), { wrapper })
 
     await waitFor(() => expect(result.current.programTableQuery.isSuccess).toBe(true))
-    // expect(result.current.programTableQuery.data).toBeDefined()
+    expect(result.current.programTableQuery.data).toBeDefined()
+  })
+  it('data has length', async () => {
+    const queryClient = new QueryClient()
+    const wrapper = ({ children }: WrapperProps) => (
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    )
+    const { result } = renderHook(() => useProgramTable(), { wrapper })
+
+    await waitFor(() => expect(result.current.programTableQuery.data?.length).toBeGreaterThan(0))
+  })
+  it('data has correct structure', async () => {
+    const queryClient = new QueryClient()
+    const wrapper = ({ children }: WrapperProps) => (
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    )
+    const { result } = renderHook(() => useProgramTable(), { wrapper })
+
+    await waitFor(() => {
+      expect(result.current.programTableQuery.data).toEqual(api.programsTable)
+    })
+    expect(result.current.programTableQuery.data?.[0].lessons.length).toBeGreaterThan(0)
   })
 })
