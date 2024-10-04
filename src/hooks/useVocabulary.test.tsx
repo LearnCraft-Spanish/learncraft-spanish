750e4b54c5596e886f1d0f089ccf42f450274f0c
VirtualBlakeWillis <bewillis101@gmail.com>
2024-10-04 14:11:47 -0500

    useVocabulary tested

--- src/hooks/useVocabulary.test.tsx
+++ src/hooks/useVocabulary.test.tsx
@@ -3,7 +3,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { renderHook, waitFor } from '@testing-library/react'
 
 import data from '../mocks/data/serverlike/mockBackendData.json'
-import quizExamples from '../mocks/data/serverlike/mockQuizExamplesTable.json'
 import { useVocabulary } from './useVocabulary'
 
 interface WrapperProps {
