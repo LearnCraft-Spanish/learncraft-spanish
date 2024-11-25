398d5c3135983593d44d4bc478c42a0b1b9ca0f5
VirtualBlakeWillis <bewillis101@gmail.com>
2024-11-25 12:05:11 -0600

    updated styling/layout

--- src/components/ExampleCreator/ExampleCreator.tsx
+++ src/components/ExampleCreator/ExampleCreator.tsx
@@ -10,7 +10,6 @@ import ExamplesTable from '../FlashcardFinder/ExamplesTable';
 import play from '../../resources/icons/play.svg';
 import pause from '../../resources/icons/pause.svg';
 import { AudioControl } from './AudioControl';
-import StyledInput from './StyledInput';
 import '../../App.css';
 import './ExampleCreator.css';
 
@@ -82,75 +81,60 @@ export default function ExampleCreator() {
             </div>
           </div>
         </div>
-        <div id="exampleForm">
-          <form onSubmit={(e) => handleAddExample(e)}>
-            <h3>Create Example</h3>
-            <div>
-              {/* <div>
-                <label id="spanishExample" className="required">
-                  Spanish Example
-                </label>
-                <input
-                  id="spanishExample"
-                  type="textarea"
-                  value={spanishExample}
-                  onChange={(e) => setSpanishExample(e.target.value)}
-                  required
-                  className="styledInput"
-                />
-              </div> */}
-              <StyledInput
-                name="spanishExample"
+        <form id="exampleForm" onSubmit={(e) => handleAddExample(e)}>
+          <h3>Create Example</h3>
+          <div>
+            <div className="inputWrapper">
+              <label id="spanishExample" className="required">
+                Spanish Example
+              </label>
+              <input
+                id="spanishExample"
+                type="textarea"
                 value={spanishExample}
                 onChange={(e) => setSpanishExample(e.target.value)}
+                required
                 className="styledInput"
               />
-              <StyledInput
-                name="spanishAudioLa"
+            </div>
+            <div className="inputWrapper">
+              <label id="englishTranslation" className="required">
+                English Translation
+              </label>
+              <input
+                id="englishTranslation"
+                type="textarea"
+                value={englishTranslation}
+                onChange={(e) => setEnglishTranslation(e.target.value)}
+                required
+                className="styledInput"
+              />
+            </div>
+          </div>
+          <div>
+            <div className="inputWrapper">
+              <label id="spanishAudioLa">Spanish Audio Link</label>
+              <input
+                id="spanishAudioLa"
+                type="url"
                 value={spanishAudioLa}
                 onChange={(e) => setSpanishAudioLa(e.target.value)}
                 className="styledInput"
               />
-
-              {/* <div>
-                <label id="spanishAudioLa">Spanish Audio Link</label>
-                <input
-                  id="spanishAudioLa"
-                  type="textarea"
-                  value={spanishAudioLa}
-                  onChange={(e) => setSpanishAudioLa(e.target.value)}
-                  className="styledInput"
-                />
-              </div> */}
             </div>
-            <div>
-              <div>
-                <label id="englishTranslation" className="required">
-                  English Translation
-                </label>
-                <input
-                  id="englishTranslation"
-                  type="textarea"
-                  value={englishTranslation}
-                  onChange={(e) => setEnglishTranslation(e.target.value)}
-                  required
-                  className="styledInput"
-                />
-              </div>
-              <div>
-                <label id="englishAudio">English Audio Link</label>
-                <input
-                  id="englishAudio"
-                  type="textarea"
-                  value={englishAudio}
-                  onChange={(e) => setEnglishAudio(e.target.value)}
-                  className="styledInput"
-                />
-              </div>
+            <div className="inputWrapper">
+              <label id="englishAudio">English Audio Link</label>
+              <input
+                id="englishAudio"
+                type="url"
+                value={englishAudio}
+                onChange={(e) => setEnglishAudio(e.target.value)}
+                className="styledInput"
+              />
             </div>
-            <button type="submit">Save Example</button>
-          </form>
-        </div>
+          </div>
+          <button type="submit">Save Example</button>
+        </form>
       </div>
 
       <div id="newExamples">
