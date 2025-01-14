e9b533aa53456decf8df81de7a7ad23001e49e9e
VirtualBlakeWillis <bewillis101@gmail.com>
2025-01-14 11:42:37 -0600

    PrivateCallsCell: proper testing

--- src/components/Coaching/WeeksRecords/Table/PrivateCallsCell.test.tsx
+++ src/components/Coaching/WeeksRecords/Table/PrivateCallsCell.test.tsx
@@ -3,37 +3,87 @@ import { render, waitFor, screen } from '@testing-library/react';
 
 import MockAllProviders from '../../../../../mocks/Providers/MockAllProviders';
 import PrivateCallsCell from './PrivateCallsCell';
-
 import mockData from '../../../../../mocks/data/serverlike/studentRecords/studentRecordsMockData';
 import { act } from 'react';
+
+// const week = mockData.calls.find((call) => call.rating > 0);
+const week = mockData.lastThreeWeeks[0];
+const call = mockData.calls.find((call) => call.relatedWeek === week.recordId);
+
+if (!call) {
+  throw new Error('No call with relatedWeek found in mock data');
+}
+if (call.recording.length < 0) {
+  throw new Error('No recording found in mock data');
+}
+
 describe('component StudentCell', () => {
-  // Write better tests, delete skipped ones
-  it.skip('renders without crashing', async () => {
+  it('default view renders without crashing', async () => {
     render(
       <MockAllProviders>
-        <PrivateCallsCell week={mockData.lastThreeWeeks[0]} />
+        <PrivateCallsCell week={week} />
       </MockAllProviders>,
     );
     await waitFor(() => {
-      // call.rating of a call related to the week record
-      expect(screen.getByText(mockData.calls[0].rating)).toBeInTheDocument();
+      expect(screen.getByText(call.rating)).toBeInTheDocument();
     });
   });
-  it('onClick, opens call popup', async () => {
-    render(
-      <MockAllProviders>
-        <PrivateCallsCell week={mockData.lastThreeWeeks[0]} />
-      </MockAllProviders>,
-    );
-    await waitFor(() => {
-      // call.rating of a call related to the week record
-      expect(screen.getByText(mockData.calls[0].rating)).toBeInTheDocument();
+  describe('contextual menu view', () => {
+    it('contextual menu view renders without crashing', async () => {
+      render(
+        <MockAllProviders>
+          <PrivateCallsCell week={week} />
+        </MockAllProviders>,
+      );
+      await waitFor(() => {
+        expect(screen.getByText(call.rating)).toBeInTheDocument();
+      });
+      // Click on the button that opens the contextual menu
+      act(() => {
+        screen.getByRole('button').click();
+      });
+      await waitFor(() => {
+        expect(screen.getByText('Rating:')).toBeInTheDocument();
+      });
     });
-    act(() => {
-      screen.getByText(mockData.calls[0].rating).click();
+
+    it('contextual menu view renders the correct data', async () => {
+      const requiredFields = ['Rating:', 'Notes:', 'Difficulties:'];
+      render(
+        <MockAllProviders>
+          <PrivateCallsCell week={week} />
+        </MockAllProviders>,
+      );
+      await waitFor(() => {
+        expect(screen.getByText(call.rating)).toBeInTheDocument();
+      });
+      // Click on the button that opens the contextual menu
+      act(() => {
+        screen.getByRole('button').click();
+      });
+      await waitFor(() => {
+        requiredFields.forEach((field) => {
+          expect(screen.getByText(field)).toBeInTheDocument();
+        });
+      });
     });
-    await waitFor(() => {
-      expect(screen.getByText('Difficulties:')).toBeInTheDocument();
+
+    it('contextual menu view renders the session documents if they exist on the call', async () => {
+      render(
+        <MockAllProviders>
+          <PrivateCallsCell week={week} />
+        </MockAllProviders>,
+      );
+      await waitFor(() => {
+        expect(screen.getByText(call.rating)).toBeInTheDocument();
+      });
+      // Click on the button that opens the contextual menu
+      act(() => {
+        screen.getByRole('button').click();
+      });
+      await waitFor(() => {
+        expect(screen.getByText('Session Documents:')).toBeInTheDocument();
+      });
     });
   });
 });
