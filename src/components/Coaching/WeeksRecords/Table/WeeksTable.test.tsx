22d2c2bdd530dcae6b9e1884563bba035b47d7bd
VirtualBlakeWillis <bewillis101@gmail.com>
2025-01-16 16:34:56 -0600

    mapped out mock data required to test WeeksTable

--- src/components/Coaching/WeeksRecords/Table/WeeksTable.test.tsx
+++ src/components/Coaching/WeeksRecords/Table/WeeksTable.test.tsx
@@ -5,6 +5,19 @@ import mockDataHardCoded from '../../../../../mocks/data/serverlike/studentRecor
 import MockAllProviders from '../../../../../mocks/Providers/MockAllProviders';
 import WeeksTable from './WeeksTable';
 
+/*
+notes for minimum mock data needed for testing this component:
+- week with no assignments,calls, or group sessions
+- 1 week with assignments
+- 1 week with calls
+- 1 week with group sessions
+- holdWeek? (1 with true, 1 with false)
+- recordsComplete? (1 with true, 1 with false)
+- 1 test with no weeks passed in
+- 1 test with 1 week passed in
+- 1 test with many weeks passed in
+- 
+*/
 describe('component WeeksTable', () => {
   // Write better tests, delete skipped ones
   it.skip('renders without crashing', async () => {
