4c0b600feaadbdb066bf7f750e22dc569fd1b14d
VirtualBlakeWillis <bewillis101@gmail.com>
2025-01-16 16:35:26 -0600

    great progress on creating useable scripts for generating all mock data for StudentRecords database

--- mocks/data/serverlike/studentRecords/scripts/fakeDataScript.ts
+++ mocks/data/serverlike/studentRecords/scripts/fakeDataScript.ts
@@ -1,52 +1,5 @@
 // import mockData from './mockData.json' assert { type: 'json' };
 // import newFakeData from './fakeData.json' assert { type: 'json' };
-/*
---- Coach List ---
-function generateFakeQBId() {
-  const format = 'NNNNNNNN.LLLL';
-  const result = format
-    .replace(/N/g, () => Math.floor(Math.random() * 10))
-    .replace(/L/g, () =>
-      // lowercase letter, or number
-      Math.random() < 0.3
-        ? Math.floor(Math.random() * 10)
-        : String.fromCharCode(97 + Math.floor(Math.random() * 26)),
-    );
-  return result;
-}
-function getNameFromEmail(email) {
-  const stepOne = email.split('@')[0].split('-').join(' ');
-  // remove any numbers at end
-  const stepTwo = stepOne.replace(/\d+$/, '');
-  // replace any - or _ with ' '
-  const stepThree = stepTwo.replace(/[-_]/g, ' ');
-  // capitalize first letter of each word
-  const stepFour = stepThree
-    .split(' ')
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(' ');
-  return stepFour;
-}
-const newCoachList = mockData.coachList.map((coach, index) => {
-  const newEmail = newFakeData.emails[index];
-  const newFullName = getNameFromEmail(newEmail);
-  // .split('@')[0]
-  // .split('-')
-  // .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-  // .join(' ');
-  return {
-    ...coach,
-    coach: newFullName.split(' ')[0],
-    user: {
-      email: newEmail,
-      qbId: generateFakeQBId(),
-      name: newFullName,
-    },
-    // ...newFakeData.coachList[index],
-  };
-});
-console.log(JSON.stringify(newCoachList));
-*/
 
 function generateLastThreeWeeksDates() {
   // Same logic  back end uses to get the date range for querying QuickBase
@@ -62,7 +15,8 @@ function generateLastThreeWeeksDates() {
     const nextSunday = new Date(nextSundayString);
     const lastSunday = new Date(lastSundayString);
     const twoSundaysAgo = new Date(twoSundaysAgoString);
-    function formatMonth(date) {
+
+    function formatMonth(date: Date) {
       const unformattedMonth = date.getMonth() + 1;
       if (unformattedMonth < 10) {
         const formattedMonth = `0${unformattedMonth.toString()}`;
@@ -72,12 +26,12 @@ function generateLastThreeWeeksDates() {
         return formattedMonth;
       }
     }
-    function formatDate(date) {
+    function formatDate(date: Date) {
       const formattedDate = date.getDate().toString();
       return formattedDate;
     }
 
-    function formatYear(date) {
+    function formatYear(date: Date) {
       const fullYear = date.getFullYear().toString();
       const formattedYear = fullYear.substring(2, 4);
       return formattedYear;
@@ -91,13 +45,13 @@ function generateLastThreeWeeksDates() {
 
     /**
     /* week name according to QuickBase Reports | how I would identify the week  | Date (as of writing Monday 1/06)
-    /* this week                                | most recent sunday - 7 days    | 12/29
-    /* last week                                | most recent sunday - 14 days   | 12/22
-    /* two weeks ago                            | most recent sunday - 21 days   | 12/15
+    /* this week                                | most recent sunday             | 12/29
+    /* last week                                | most recent sunday - 7 days    | 12/22
+    /* two weeks ago                            | most recent sunday - 14 days   | 12/15
     /* upcoming week                            | most recent sunday (this week) | 1/05
     */
   }
-  // console.log(makeDateRange());
+  return makeDateRange();
 }
 
 generateLastThreeWeeksDates();
---
+++ mocks/data/serverlike/studentRecords/scripts/generateCourseList.ts
@@ -0,0 +1,99 @@
+const courseList = [
+  {
+    recordId: 1,
+    name: 'Membership Program',
+    membershipType: 'Basic Membership Program',
+    weeklyPrivateCalls: 0,
+    hasGroupCalls: false,
+  },
+  {
+    recordId: 2,
+    name: 'Group Coaching (formerly Alumni)',
+    membershipType: 'Group Conversation',
+    weeklyPrivateCalls: 0,
+    hasGroupCalls: true,
+  },
+  {
+    recordId: 14,
+    name: 'Standard',
+    membershipType: 'Private Coaching',
+    weeklyPrivateCalls: 1,
+    hasGroupCalls: true,
+  },
+  {
+    recordId: 15,
+    name: 'Premier',
+    membershipType: 'Private Coaching',
+    weeklyPrivateCalls: 2,
+    hasGroupCalls: true,
+  },
+  {
+    recordId: 16,
+    name: '1-Month Challenge',
+    membershipType: '1MC',
+    weeklyPrivateCalls: 0,
+    hasGroupCalls: false,
+  },
+  // {
+  //   recordId: 21,
+  //   name: 'Listening Comprehension',
+  //   membershipType: 'Listening Comprehension',
+  //   weeklyPrivateCalls: 0,
+  //   hasGroupCalls: false,
+  // },
+  {
+    recordId: 22,
+    name: 'LCS Cohort',
+    membershipType: 'LCS Cohort',
+    weeklyPrivateCalls: 1,
+    hasGroupCalls: true,
+  },
+  {
+    recordId: 23,
+    name: 'Standard: Private-Only',
+    membershipType: 'Weekly 1:1 Coaching ',
+    weeklyPrivateCalls: 1,
+    hasGroupCalls: false,
+  },
+  {
+    recordId: 24,
+    name: 'Premier: Private-Only',
+    membershipType: '',
+    weeklyPrivateCalls: 2,
+    hasGroupCalls: false,
+  },
+  // {
+  //   recordId: 25,
+  //   name: '1-Month Challenge Deluxe',
+  //   membershipType: '1MC Conversation',
+  //   weeklyPrivateCalls: 0,
+  //   hasGroupCalls: false,
+  // },
+  // {
+  //   recordId: 26,
+  //   name: 'Ser/Estar Mini Course',
+  //   membershipType: '',
+  //   weeklyPrivateCalls: 0,
+  //   hasGroupCalls: false,
+  // },
+  {
+    recordId: 27,
+    name: '1MC With Coaching',
+    membershipType: '',
+    weeklyPrivateCalls: 1,
+    hasGroupCalls: false,
+  },
+  // {
+  //   recordId: 28,
+  //   name: 'Listening Comprehension with Coaching',
+  //   membershipType: 'Listening Comprehension',
+  //   weeklyPrivateCalls: 0,
+  //   hasGroupCalls: false,
+  // },
+];
+
+function generateCourseList() {
+  return courseList;
+}
+
+export default generateCourseList;
