e364b7a6b6fcb4e08a361c9221931df9f9bdac5b
ClassicalMoser <69487098+ClassicalMoser@users.noreply.github.com>
2025-01-20 10:51:19 -0600

    import vite config to vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,42 +1,47 @@
-import react from '@vitejs/plugin-react';
-// vitest.config.ts
+import { mergeConfig } from 'vite';
+import type { ConfigEnv, UserConfig } from 'vite';
 import { defineConfig } from 'vitest/config';
+import viteConfig from './vite.config';
 
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './tests/setupTests',
-    include: ['**/*.{test,spec}.{js,ts,jsx,tsx}'],
-    exclude: [
-      '**/node_modules/**',
-      '**/mocks/**',
-      'Coaching.jsx',
-      '**/functions/**',
-      'useAuth.ts',
-    ],
-    mockReset: true,
-    clearMocks: true,
-    restoreMocks: true,
-    coverage: {
-      exclude: [
-        '**/node_modules/**',
-        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*',
-        '**/[.]**',
-        'test?(s)/**',
-        'test?(-*).?(c|m)[jt]s?(x)',
-        '**/*{.,-}{test,spec,bench,benchmark}?(-d).?(c|m)[jt]s?(x)',
-        '**/mocks/**',
-        '**/Coaching.jsx',
-        '**/functions/**',
-        '**/useAuth.ts',
-        '**/interfaceDefinitions.tsx',
-        '**/vite-env.d.ts',
-        '**/react-app-env.d.ts',
-        '**/Coaching/old/**',
-        '**/Coaching/*.js',
-      ],
-    },
-  },
-});
+export default defineConfig((configEnv: ConfigEnv) =>
+  mergeConfig(
+    viteConfig(configEnv),
+    defineConfig({
+      test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './tests/setupTests',
+        include: ['**/*.{test,spec}.{js,ts,jsx,tsx}'],
+        exclude: [
+          '**/node_modules/**',
+          '**/mocks/**',
+          'Coaching.jsx',
+          '**/functions/**',
+          'useAuth.ts',
+        ],
+        mockReset: true,
+        clearMocks: true,
+        restoreMocks: true,
+        coverage: {
+          exclude: [
+            '**/node_modules/**',
+            '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*',
+            '**/[.]**',
+            'test?(s)/**',
+            'test?(-*).?(c|m)[jt]s?(x)',
+            '**/*{.,-}{test,spec,bench,benchmark}?(-d).?(c|m)[jt]s?(x)',
+            '**/mocks/**',
+            '**/Coaching.jsx',
+            '**/functions/**',
+            '**/useAuth.ts',
+            '**/interfaceDefinitions.tsx',
+            '**/vite-env.d.ts',
+            '**/react-app-env.d.ts',
+            '**/Coaching/old/**',
+            '**/Coaching/*.js',
+          ],
+        },
+      },
+    }),
+  ),
+);
