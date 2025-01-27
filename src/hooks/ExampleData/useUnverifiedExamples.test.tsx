0598a67b54033a1586f72d86d515b811c8e1bf77
VirtualBlakeWillis <bewillis101@gmail.com>
2025-01-27 14:13:19 -0700

    Testing Updates: when these tests run, the variable in the expect() is directly referencing the result.current of the rendered hook (instead of a const variable declared before the hook has sucessfully rendered with data)

--- src/hooks/ExampleData/useUnverifiedExamples.test.tsx
+++ src/hooks/ExampleData/useUnverifiedExamples.test.tsx
@@ -14,23 +14,30 @@ describe('useUnverifiedExamples', () => {
       wrapper: MockAllProviders,
     });
 
-    const query = result.current.verifiedExamplesQuery;
-    await waitFor(() => expect(query.isSuccess).toBe(true), {
-      timeout: 3000,
-      interval: 200,
-    });
-    expect(query.data).toBeDefined();
+    await waitFor(
+      () => expect(result.current.verifiedExamplesQuery.isSuccess).toBe(true),
+      {
+        timeout: 3000,
+        interval: 200,
+      },
+    );
+    expect(result.current.verifiedExamplesQuery.data).toBeDefined();
   });
 
   it('data has length', async () => {
     const { result } = renderHook(() => useVerifiedExamples(), {
       wrapper: MockAllProviders,
     });
-    const query = result.current.verifiedExamplesQuery;
-    await waitFor(() => expect(query.data?.length).toBeGreaterThan(0), {
-      timeout: 3000,
-      interval: 200,
-    });
+    await waitFor(
+      () =>
+        expect(
+          result.current.verifiedExamplesQuery.data?.length,
+        ).toBeGreaterThan(0),
+      {
+        timeout: 3000,
+        interval: 200,
+      },
+    );
   });
 
   describe('when user is not an admin or student', () => {
@@ -41,15 +48,14 @@ describe('useUnverifiedExamples', () => {
       const { result } = renderHook(() => useVerifiedExamples(), {
         wrapper: MockAllProviders,
       });
-      const query = result.current.verifiedExamplesQuery;
       await waitFor(
         () => {
-          expect(query.isSuccess).toBe(false);
+          expect(result.current.verifiedExamplesQuery.isSuccess).toBe(false);
         },
         { timeout: 10000, interval: 200 },
       );
-      expect(query.isSuccess).toBe(false);
-      expect(query.data).toEqual(undefined);
+      expect(result.current.verifiedExamplesQuery.isSuccess).toBe(false);
+      expect(result.current.verifiedExamplesQuery.data).toEqual(undefined);
     });
   });
 });
--- src/hooks/ExampleData/useVerifiedExamples.test.tsx
+++ src/hooks/ExampleData/useVerifiedExamples.test.tsx
@@ -13,23 +13,30 @@ describe('useVerifiedExamples', () => {
     const { result } = renderHook(() => useVerifiedExamples(), {
       wrapper: MockAllProviders,
     });
-    const query = result.current.verifiedExamplesQuery;
-    await waitFor(() => expect(query.isSuccess).toBe(true), {
-      timeout: 3000,
-      interval: 200,
-    });
-    expect(query.data).toBeDefined();
+    await waitFor(
+      () => expect(result.current.verifiedExamplesQuery.isSuccess).toBe(true),
+      {
+        timeout: 3000,
+        interval: 200,
+      },
+    );
+    expect(result.current.verifiedExamplesQuery.data).toBeDefined();
   });
 
   it('data has length', async () => {
     const { result } = renderHook(() => useVerifiedExamples(), {
       wrapper: MockAllProviders,
     });
-    const query = result.current.verifiedExamplesQuery;
-    await waitFor(() => expect(query.data?.length).toBeGreaterThan(0), {
-      timeout: 3000,
-      interval: 200,
-    });
+    await waitFor(
+      () =>
+        expect(
+          result.current.verifiedExamplesQuery.data?.length,
+        ).toBeGreaterThan(0),
+      {
+        timeout: 3000,
+        interval: 200,
+      },
+    );
   });
   describe('when user is not an admin or student', () => {
     beforeEach(() => {
@@ -39,15 +46,14 @@ describe('useVerifiedExamples', () => {
       const { result } = renderHook(() => useVerifiedExamples(), {
         wrapper: MockAllProviders,
       });
-      const query = result.current.verifiedExamplesQuery;
       await waitFor(
         () => {
-          expect(query.isSuccess).toBe(false);
+          expect(result.current.verifiedExamplesQuery.isSuccess).toBe(false);
         },
         { timeout: 10000, interval: 200 },
       );
-      expect(query.isSuccess).toBe(false);
-      expect(query.data).toEqual(undefined);
+      expect(result.current.verifiedExamplesQuery.isSuccess).toBe(false);
+      expect(result.current.verifiedExamplesQuery.data).toEqual(undefined);
     });
   });
 });
