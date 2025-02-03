2c5879deaaf380fd4b1c797781052de2d26aa0a5
VirtualBlakeWillis <bewillis101@gmail.com>
2025-02-03 13:50:50 -0700

    Modal: works, implemented in AssignmentsCell

--- src/providers/ModalProvider.tsx
+++ src/providers/ModalProvider.tsx
@@ -1,31 +1,48 @@
-import React, { useCallback, useMemo, useRef, useState } from 'react';
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
+
+import { useContextualMenu } from 'src/hooks/useContextualMenu';
+
 import type {
   ModalContentProps,
   ModalContextType,
 } from '../context/ModalContext';
 import ModalContext from '../context/ModalContext';
 import Modal from '../components/Modal/Modal';
+
 export const ModalProvider: React.FC<{
   children: React.ReactNode;
 }> = ({ children }) => {
+  const { updateDisableClickOutside } = useContextualMenu();
+
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [modalProps, setModalProps] = useState<ModalContentProps | null>(null);
-  const openModal = useCallback((props: ModalContentProps) => {
-    if (
-      !props ||
-      props.title.length === 0 ||
-      props.body.length === 0 ||
-      !props.type
-    ) {
-      throw new Error('Modal props must have a title body, & type');
-    }
-    setModalProps(props);
-    setIsOpen(true);
-  }, []);
+  const openModal = useCallback(
+    (props: ModalContentProps) => {
+      if (
+        !props ||
+        props.title.length === 0 ||
+        props.body.length === 0 ||
+        !props.type
+      ) {
+        throw new Error('Modal props must have a title body, & type');
+      }
+      setModalProps(props);
+      setIsOpen(true);
+      updateDisableClickOutside(true);
+    },
+    [updateDisableClickOutside],
+  );
   const closeModal = useCallback(() => {
     setIsOpen(false);
     setModalProps(null);
-  }, []);
+    updateDisableClickOutside(false);
+  }, [updateDisableClickOutside]);
 
   const value: ModalContextType = useMemo(
     () => ({
