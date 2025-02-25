5d3587f7ffad99956366467ff9791ece67f5d1fa
VirtualBlakeWillis <bewillis101@gmail.com>
2025-02-25 15:14:08 -0600

    CI

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,5 +1,5 @@
-import type { ModalContextType } from '../context/ModalContext';
 import { useContext } from 'react';
+import type { ModalContextType } from '../context/ModalContext';
 import ModalContext from '../context/ModalContext';
 
 export const useModal = (): ModalContextType => {
