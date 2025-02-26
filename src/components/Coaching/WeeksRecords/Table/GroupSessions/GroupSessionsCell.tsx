78fc40446de5821eff85a15caff8de572737bc22
VirtualBlakeWillis <bewillis101@gmail.com>
2025-02-26 14:40:36 -0600

    GroupSessionsTopicFieldOptions is sorted alphabetically

--- src/components/Coaching/WeeksRecords/Table/GroupSessions/GroupSessionsCell.tsx
+++ src/components/Coaching/WeeksRecords/Table/GroupSessions/GroupSessionsCell.tsx
@@ -507,7 +507,19 @@ function GroupSessionView({
           value={topic}
           onChange={setTopic}
           editMode={editMode}
-          options={groupSessionsTopicFieldOptionsQuery.data ?? []}
+          options={
+            groupSessionsTopicFieldOptionsQuery.data
+              ? groupSessionsTopicFieldOptionsQuery.data.sort((a, b) => {
+                  if (a && b) {
+                    const aString = a.replace(/^"/g, '').toLowerCase();
+                    const bString = b.replace(/^"/g, '').toLowerCase();
+                    if (aString > bString) return 1;
+                    else return -1;
+                  }
+                  return 0;
+                })
+              : []
+          }
           label="Topic"
         />
         <TextAreaInput
