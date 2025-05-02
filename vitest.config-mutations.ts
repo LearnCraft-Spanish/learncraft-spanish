b095aebf0e143cfa44caf83ee96e00d1af12eb47
VirtualBlakeWillis <bewillis101@gmail.com>
2025-05-02 11:09:39 -0500

    passing stryker a custom vitest config

---
+++ vitest.config-mutations.ts
@@ -0,0 +1,53 @@
+import type { ConfigEnv } from 'vite';
+import { mergeConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
+import viteConfig from './vite.config';
+
+export default defineConfig((configEnv: ConfigEnv) =>
+  mergeConfig(
+    viteConfig(configEnv),
+    defineConfig({
+      test: {
+        silent: true,
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './tests/setupTests',
+        include: ['**/hexagon/**/*.{test,spec}.{js,ts,jsx,tsx}'],
+        exclude: [
+          '**/node_modules/**',
+          '**/mocks/**',
+          '**/.stryker-tmp/**',
+          'useAuth.ts',
+        ],
+        mockReset: true,
+        clearMocks: true,
+        restoreMocks: true,
+        testTimeout: 5000,
+        coverage: {
+          exclude: [
+            '**/!(hexagon)/**',
+            '**/*{.,-}{test,spec,bench,benchmark}?(-d).?(c|m)[jt]s?(x)',
+            '**/{index,types}?(-d).?(c|m)[jt]s?(x)',
+            '**/[.]**',
+            '**/components/**',
+            '**/.stryker-tmp/**',
+            '**/node_modules/**',
+            '**/assets/**',
+            '**/tests/**',
+            '**/mocks/**',
+            '**/providers/**',
+            '**/types/**',
+            '**/METRICS/**',
+            '**/src/index.tsx',
+            '**/react-app-env.d.ts',
+            '**/test?(-*).?(c|m)[jt]s?(x)',
+            '**/test?(s)/**',
+            '**/useAuth.ts',
+            '**/vite-env.d.ts',
+            '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*',
+          ],
+        },
+      },
+    }),
+  ),
+);
