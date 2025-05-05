b172f202b7bc0033d5b217ac8ed110c39f6470e3
ClassicalMoser <69487098+ClassicalMoser@users.noreply.github.com>
2025-05-05 11:48:23 -0500

    validation

--- src/hooks/useContextualMenu.ts
+++ src/hooks/useContextualMenu.ts
@@ -1,9 +1,9 @@
 import type { ContextualMenuContextType } from '../context/ContextualMenuContext';
-import { useContext } from 'react';
+import { use } from 'react';
 import ContextualMenuContext from '../context/ContextualMenuContext';
 
 export const useContextualMenu = (): ContextualMenuContextType => {
-  const context = useContext(ContextualMenuContext);
+  const context = use(ContextualMenuContext);
   if (!context) {
     throw new Error(
       'useContextualMenu must be used within a ContextualMenuProvider',
