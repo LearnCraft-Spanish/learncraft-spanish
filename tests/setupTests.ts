013d2b07460d73e5392b1f08e27376116893a119
ClassicalMoser <69487098+ClassicalMoser@users.noreply.github.com>
2025-05-05 12:26:39 -0500

    Split hexagonal testing

--- tests/setupTests.ts
+++ tests/setupTests.ts
@@ -1,7 +1,4 @@
 import { server } from 'mocks/api/server';
-import { callMockSubcategoryAdapter } from 'src/hexagon/application/adapters/subcategoryAdapter.mock';
-
-import { callMockVocabularyAdapter } from 'src/hexagon/application/adapters/vocabularyAdapter.mock';
 import { afterAll, afterEach, beforeAll, beforeEach, vi } from 'vitest';
 import { setupMockAuth } from './setupMockAuth';
 import '@testing-library/jest-dom';
@@ -13,19 +10,8 @@ import '@testing-library/jest-dom';
 // Mock the useAuth hook, but leave the mock return configurable per test
 vi.mock('src/hooks/useAuth');
 
-const setupAdapterMocks = () => {
-  vi.mock('@application/adapters/vocabularyAdapter', () => ({
-    useVocabularyAdapter: callMockVocabularyAdapter,
-  }));
-
-  vi.mock('@application/adapters/subcategoryAdapter', () => ({
-    useSubcategoryAdapter: callMockSubcategoryAdapter,
-  }));
-};
-
 beforeEach(() => {
   setupMockAuth();
-  setupAdapterMocks();
 });
 
 // Open the MSW server before all tests
