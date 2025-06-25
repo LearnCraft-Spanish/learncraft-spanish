11b95672f3ca6ad909272c691e5372afdb011a12
ClassicalMoser <69487098+ClassicalMoser@users.noreply.github.com>
2025-06-25 15:28:56 -0500

    Fix types and rename file

--- src/hexagon/application/coordinators/hooks/useActiveStudent.ts
+++ src/hexagon/application/coordinators/hooks/useActiveStudent.ts
@@ -10,6 +10,7 @@ interface UseActiveStudentReturnType {
   appUser: AppUser | null;
   isLoading: boolean;
   error: Error | null;
+  isOwnUser: boolean;
   changeActiveStudent: (newEmail: string | null) => void;
 }
 
@@ -82,7 +83,7 @@ export function useActiveStudent(): UseActiveStudentReturnType {
   });
 
   const returnValue = useMemo(
-    () => ({
+    (): UseActiveStudentReturnType => ({
       appUser: appUser || null,
       isLoading,
       error,
