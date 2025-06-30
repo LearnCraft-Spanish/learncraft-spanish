b5853fd1f0a5169b9c98b7a3a0275e939cfe10f7
VirtualBlakeWillis <bewillis101@gmail.com>
2025-06-30 16:43:08 -0500

    useFilteredExamples infrastructure (mainly for testing backend logic) created, not implemented

---
+++ src/hexagon/application/ports/examplePort.ts
@@ -0,0 +1,22 @@
+import type {
+  ExampleWithVocabulary,
+  SkillTag,
+} from '@LearnCraft-Spanish/shared';
+
+export interface ExamplePort {
+  getFilteredExamples: ({
+    courseId,
+    toLessonNumber,
+    fromLessonNumber,
+    spanglishOnly,
+    audioOnly,
+    skillTags,
+  }: {
+    courseId: number;
+    toLessonNumber: number;
+    fromLessonNumber?: number;
+    spanglishOnly?: boolean;
+    audioOnly?: boolean;
+    skillTags?: SkillTag[];
+  }) => Promise<ExampleWithVocabulary[]>;
+}
---
+++ src/hexagon/infrastructure/exampleInfrastructure.ts
@@ -0,0 +1,35 @@
+import type {
+  ExampleWithVocabulary,
+  SkillTag,
+} from '@LearnCraft-Spanish/shared';
+import type { AuthPort } from '../application/ports/authPort';
+import type { ExamplePort } from '../application/ports/examplePort';
+import { createHttpClient } from '@infrastructure/http/client';
+import { queryExamplesEndpoint } from '@LearnCraft-Spanish/shared';
+
+export function createExampleInfrastructure(
+  apiUrl: string,
+  auth: AuthPort,
+): ExamplePort {
+  const httpClient = createHttpClient(apiUrl, auth);
+
+  return {
+    getFilteredExamples: async (params: {
+      courseId: number;
+      toLessonNumber: number;
+      fromLessonNumber?: number;
+      spanglishOnly?: boolean;
+      audioOnly?: boolean;
+      skillTags?: SkillTag[];
+    }) => {
+      const response = await httpClient.post<ExampleWithVocabulary[]>(
+        queryExamplesEndpoint.path,
+        queryExamplesEndpoint.requiredScopes,
+        {
+          data: params,
+        },
+      );
+      return response;
+    },
+  };
+}
