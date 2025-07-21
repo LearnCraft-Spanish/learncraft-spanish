6c2d4c31fb7c3e9c9716390939211e44c7c446b4
VirtualBlakeWillis <bewillis101@gmail.com>
2025-07-21 12:42:31 -0500

    ExampleListItem render optomized

--- src/hexagon/interface/components/ExampleListItem/units/MoreInfoViewExample.tsx
+++ src/hexagon/interface/components/ExampleListItem/units/MoreInfoViewExample.tsx
@@ -9,42 +9,44 @@ export default function MoreInfoViewExample({
   isOpen: boolean;
 }) {
   return (
-    <div className={`moreInfoView ${isOpen ? 'open' : 'closed'}`}>
-      <div className="vocabTagsSide">
-        <h3>Vocab Tags</h3>
-        {/* This should be a list of Skill Tag display objects, not just strings */}
-        <div className="vocabTagsList">
-          {example.vocabulary.map((vocab) => (
-            <div className="vocabTag" key={vocab.id}>
-              {vocab.word}
-            </div>
-          ))}
+    isOpen && (
+      <div className={`moreInfoView ${isOpen ? 'open' : 'closed'}`}>
+        <div className="vocabTagsSide">
+          <h3>Vocab Tags</h3>
+          {/* This should be a list of Skill Tag display objects, not just strings */}
+          <div className="vocabTagsList">
+            {example.vocabulary.map((vocab) => (
+              <div className="vocabTag" key={vocab.id}>
+                {vocab.word}
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-      <div className="moreInfoSide">
-        {example.spanishAudio && (
-          <div className="audioWrapper">
-            <div className="audioLabel">Spanish Audio</div>
-            <AudioControl audioLink={example.spanishAudio} />
-          </div>
-        )}
-        {example.englishAudio && (
-          <div className="audioWrapper">
-            <div className="audioLabel">English Audio</div>
-            <AudioControl audioLink={example.englishAudio} />
-          </div>
-        )}
-      </div>
-      <div className="moreInfoSide">
-        <div className="labelsSection">
-          {example.spanglish && (
-            <div className="label spanglish">spanglish</div>
-          )}
+        <div className="moreInfoSide">
           {example.spanishAudio && (
-            <div className="label audio">audio flashcard</div>
+            <div className="audioWrapper">
+              <div className="audioLabel">Spanish Audio</div>
+              <AudioControl audioLink={example.spanishAudio} />
+            </div>
+          )}
+          {example.englishAudio && (
+            <div className="audioWrapper">
+              <div className="audioLabel">English Audio</div>
+              <AudioControl audioLink={example.englishAudio} />
+            </div>
           )}
         </div>
+        <div className="moreInfoSide">
+          <div className="labelsSection">
+            {example.spanglish && (
+              <div className="label spanglish">spanglish</div>
+            )}
+            {example.spanishAudio && (
+              <div className="label audio">audio flashcard</div>
+            )}
+          </div>
+        </div>
       </div>
-    </div>
+    )
   );
 }
