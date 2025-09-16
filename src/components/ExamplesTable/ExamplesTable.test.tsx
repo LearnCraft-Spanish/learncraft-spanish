f4ac80f610f241e04a545f065e49430b05772299
ClassicalMoser <69487098+ClassicalMoser@users.noreply.github.com>
2025-09-16 15:27:47 -0500

    Restore accidentally deleted files

---
+++ src/components/ExamplesTable/ExamplesTable.test.tsx
@@ -0,0 +1,212 @@
+import type { DisplayOrder, Flashcard } from 'src/types/interfaceDefinitions';
+import { render, renderHook, screen, waitFor } from '@testing-library/react';
+import serverlikeData from 'mocks/data/serverlike/serverlikeData';
+
+import { getAuthUserFromEmail } from 'mocks/data/serverlike/userTable';
+import MockAllProviders from 'mocks/Providers/MockAllProviders';
+import React, { act } from 'react';
+import { overrideAuthAndAppUser } from 'src/hexagon/testing/utils/overrideAuthAndAppUser';
+import { useStudentFlashcards } from 'src/hooks/UserData/useStudentFlashcards';
+import { beforeEach, describe, expect, it } from 'vitest';
+import ExamplesTable from './ExamplesTable';
+
+const verifiedExamplesTable = serverlikeData().api.verifiedExamplesTable;
+
+// Helper Functions
+const mockDataSource = verifiedExamplesTable.slice(0, 15);
+
+function makeDisplayOrder(examples: Flashcard[]): DisplayOrder[] {
+  const newDisplayOrder = examples.map((example) => {
+    const displayOrder: DisplayOrder = { recordId: example.recordId };
+    return displayOrder;
+  });
+  return newDisplayOrder;
+}
+
+const fullDisplayOrder = makeDisplayOrder(mockDataSource);
+
+async function getUnknownExample() {
+  const { result } = renderHook(() => useStudentFlashcards(), {
+    wrapper: MockAllProviders,
+  });
+  await waitFor(() =>
+    expect(result.current.flashcardDataQuery.isSuccess).toBe(true),
+  );
+  const unknownExample = verifiedExamplesTable.find(
+    (example) => result.current.exampleIsCollected(example.recordId) === false,
+  );
+  if (!unknownExample) {
+    throw new Error('No unknown examples found');
+  }
+  return unknownExample;
+}
+
+async function getKnownExample() {
+  const { result } = renderHook(() => useStudentFlashcards(), {
+    wrapper: MockAllProviders,
+  });
+  await waitFor(() =>
+    expect(result.current.flashcardDataQuery.isSuccess).toBe(true),
+  );
+  const knownExample = verifiedExamplesTable.find(
+    (example) =>
+      result.current.exampleIsCollected(example.recordId) === true &&
+      !result.current.exampleIsCustom(example.recordId),
+  );
+  if (!knownExample) {
+    throw new Error('No known examples found');
+  }
+  return knownExample;
+}
+describe('renders without crashing', () => {
+  it('renders without crashing', async () => {
+    const examplesToDisplay = verifiedExamplesTable.slice(0, 1);
+    render(
+      <ExamplesTable
+        dataSource={verifiedExamplesTable}
+        displayOrder={fullDisplayOrder}
+      />,
+      { wrapper: MockAllProviders },
+    );
+    await waitFor(
+      () => {
+        expect(
+          screen.getByText(examplesToDisplay[0].englishTranslation),
+        ).toBeInTheDocument();
+      },
+      { timeout: 7500 },
+    );
+  });
+});
+
+describe('user is not a student', () => {
+  it('student buttons: Add/Adding.../Remove are not displayed', async () => {
+    overrideAuthAndAppUser(
+      {
+        authUser: getAuthUserFromEmail('admin-empty-role@fake.not')!,
+        isAuthenticated: true,
+        isAdmin: true,
+        isCoach: false,
+        isStudent: false,
+        isLimited: false,
+      },
+      {
+        isOwnUser: true,
+      },
+    );
+    render(
+      <MockAllProviders>
+        <ExamplesTable
+          dataSource={mockDataSource}
+          displayOrder={fullDisplayOrder}
+        />
+        ,
+      </MockAllProviders>,
+    );
+    await waitFor(() => {
+      expect(
+        screen.getByText(mockDataSource[3].englishTranslation),
+      ).toBeInTheDocument();
+    });
+
+    expect(screen.queryByText('Add')).not.toBeInTheDocument();
+    expect(screen.queryByText('Adding...')).not.toBeInTheDocument();
+    expect(screen.queryByText('Remove')).not.toBeInTheDocument();
+  });
+});
+
+describe('user is a student', () => {
+  beforeEach(() => {
+    overrideAuthAndAppUser(
+      {
+        authUser: getAuthUserFromEmail('student-admin@fake.not')!,
+        isAuthenticated: true,
+        isAdmin: false,
+        isCoach: false,
+        isStudent: true,
+        isLimited: false,
+      },
+      {
+        isOwnUser: true,
+      },
+    );
+  });
+  describe('example is unknown', () => {
+    beforeEach(async () => {
+      const unknownExample = await getUnknownExample();
+      render(
+        <ExamplesTable
+          dataSource={[unknownExample]}
+          displayOrder={[{ recordId: unknownExample.recordId }]}
+        />,
+        { wrapper: MockAllProviders },
+      );
+      await waitFor(() => {
+        expect(
+          screen.getByText(unknownExample.englishTranslation),
+        ).toBeInTheDocument();
+      });
+    });
+
+    it('"Add" button is displayed', async () => {
+      await waitFor(() => {
+        expect(screen.getByText('Add')).toBeInTheDocument();
+        expect(screen.queryByText('Adding...')).not.toBeInTheDocument();
+        expect(screen.queryByText('Remove')).not.toBeInTheDocument();
+      });
+    });
+    it.skip('on click, "Adding..." is displayed', async () => {
+      // believed to be a limitation of the mock server
+      // becuase we are adding, but the server is not actually adding
+      // so the status gets reverted? after the optomistic update
+      await waitFor(() => {
+        expect(screen.getByText('Add')).toBeInTheDocument();
+      });
+      act(() => {
+        screen.getByText('Add').click();
+      });
+      await waitFor(() => {
+        expect(screen.queryByText('Add')).not.toBeInTheDocument();
+        expect(screen.getByText('Adding...')).toBeInTheDocument();
+      });
+    });
+  });
+
+  describe('example is known', () => {
+    beforeEach(async () => {
+      const knownExample = await getKnownExample();
+      render(
+        <ExamplesTable
+          dataSource={[knownExample]}
+          displayOrder={[{ recordId: knownExample.recordId }]}
+        />,
+        { wrapper: MockAllProviders },
+      );
+      await waitFor(() => {
+        expect(
+          screen.getByText(knownExample.englishTranslation),
+        ).toBeInTheDocument();
+      });
+    });
+
+    it('example is known, Remove button is displayed', async () => {
+      await waitFor(() => {
+        expect(screen.getByText('Remove')).toBeInTheDocument();
+        expect(screen.queryByText('Add')).not.toBeInTheDocument();
+        expect(screen.queryByText('Adding...')).not.toBeInTheDocument();
+      });
+    });
+    it('on click, "Add" is displayed (optomistic inteface update', async () => {
+      await waitFor(() => {
+        expect(screen.getByText('Remove')).toBeInTheDocument();
+      });
+      act(() => {
+        screen.getByText('Remove').click();
+      });
+      await waitFor(() => {
+        expect(screen.queryByText('Remove')).not.toBeInTheDocument();
+        expect(screen.getByText('Add')).toBeInTheDocument();
+      });
+    });
+  });
+});
---
+++ src/components/ExamplesTable/ExamplesTable.tsx
@@ -0,0 +1,173 @@
+import type { DisplayOrder, Flashcard } from 'src/types/interfaceDefinitions';
+
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { Pagination } from 'src/components/Table/components';
+import ExampleListItem from './ExampleListItem';
+
+import './ExamplesTable.scss';
+
+interface ExamplesTableProps {
+  dataSource: Flashcard[];
+  displayOrder: DisplayOrder[];
+  showSpanglishLabel?: boolean;
+  forceShowVocab?: boolean;
+  selectFunction?: (recordId: number | null) => void;
+  sorted?: boolean;
+  studentContext?: boolean;
+}
+
+export default function ExamplesTable({
+  dataSource,
+  displayOrder,
+  showSpanglishLabel = false,
+  sorted = false,
+  forceShowVocab = false,
+  selectFunction = undefined,
+  studentContext = true,
+}: ExamplesTableProps) {
+  const [page, setPage] = useState(1);
+  const [selectedExampleId, setSelectedExampleId] = useState<number | null>(
+    null,
+  );
+  const itemsPerPage = 50;
+  const maxPage = Math.ceil(displayOrder.length / itemsPerPage);
+
+  const displayOrderSegment = displayOrder.slice(
+    (page - 1) * itemsPerPage,
+    page * itemsPerPage,
+  );
+
+  const nextPage = useCallback(() => {
+    if (page >= maxPage) {
+      return;
+    }
+    setPage(page + 1);
+  }, [page, maxPage]);
+
+  const previousPage = useCallback(() => {
+    if (page <= 1) {
+      return;
+    }
+    setPage(page - 1);
+  }, [page]);
+
+  const selectExample = useMemo(() => {
+    if (selectFunction) {
+      return (recordId: number | null) => {
+        selectFunction(recordId);
+        setSelectedExampleId(recordId);
+      };
+    }
+    return undefined;
+  }, [selectFunction]);
+
+  const getExampleById = useCallback(
+    (recordId: number) => {
+      const foundExample = dataSource.find(
+        (example) => example.recordId === recordId,
+      );
+      if (!foundExample?.recordId) {
+        console.error('No example found with id: ', recordId);
+        return null;
+      }
+      return foundExample;
+    },
+    [dataSource],
+  );
+
+  const displayExamplesWithAudio = useMemo(() => {
+    const audioExamples = displayOrder.filter(
+      (displayOrderItem: DisplayOrder) => {
+        const example = getExampleById(displayOrderItem.recordId);
+        if (example?.spanishAudioLa) {
+          if (example.spanishAudioLa.length > 0) {
+            return true;
+          }
+          return false;
+        }
+        return false;
+      },
+    );
+    return audioExamples?.length || 0;
+  }, [displayOrder, getExampleById]);
+
+  // called when user clicks 'Copy as Table' button
+  // copies sentences in a table format to be pasted into a google doc or excel sheet
+  function copyTable() {
+    if (!displayOrder.length) {
+      return null;
+    }
+    const headers = 'ID\tSpanish\tEnglish\tAudio_Link\n';
+    const table = displayOrder
+      .map((displayOrderObject) => {
+        const foundExample = getExampleById(displayOrderObject.recordId);
+        if (!foundExample) {
+          return '';
+        }
+        return `${foundExample.recordId}\t\
+            ${foundExample.spanishExample}\t\
+            ${foundExample.englishTranslation}\t\
+            ${foundExample.spanishAudioLa}\n`;
+      })
+      .join('');
+
+    const copiedText = headers + table;
+    navigator.clipboard.writeText(copiedText);
+  }
+
+  useEffect(() => {
+    if (!sorted || maxPage === 1) {
+      setPage(1);
+    }
+  }, [displayOrder, sorted, maxPage]);
+
+  return (
+    <div className="examplesTable">
+      <div className="buttonBox">
+        <button type="button" onClick={copyTable}>
+          Copy Table
+        </button>
+        <div className="displayExamplesDescription">
+          <h4>
+            {`${displayOrder.length} flashcards showing (
+                ${displayExamplesWithAudio} with audio)`}
+          </h4>
+        </div>
+      </div>
+      <Pagination
+        page={page}
+        maxPage={maxPage}
+        nextPage={nextPage}
+        previousPage={previousPage}
+      />
+      {/* unnessessary id? */}
+      <div id="examplesTableBody">
+        {displayOrderSegment.map((displayOrder) => {
+          const id = displayOrder.recordId;
+          const exampleData = getExampleById(id);
+          if (!exampleData) {
+            return null;
+          } else
+            return (
+              <ExampleListItem
+                key={displayOrder.recordId}
+                example={exampleData}
+                showSpanglishLabel={showSpanglishLabel}
+                forceShowVocab={forceShowVocab}
+                selectExample={selectExample}
+                selectedExampleId={selectedExampleId}
+                studentContext={studentContext}
+              />
+            );
+        })}
+      </div>
+
+      <Pagination
+        page={page}
+        maxPage={maxPage}
+        nextPage={nextPage}
+        previousPage={previousPage}
+      />
+    </div>
+  );
+}
