11fd67c8161a12e62aad04dfc76a3f3d5167291a
VirtualBlakeWillis <bewillis101@gmail.com>
2025-09-25 15:37:22 -0500

    subjunctive challenge is only visible to Admins and students in that program

--- src/hexagon/application/queries/useCoursesWithLessons.ts
+++ src/hexagon/application/queries/useCoursesWithLessons.ts
@@ -1,14 +1,25 @@
 import { useAuthAdapter } from '@application/adapters/authAdapter';
 import { useCourseAdapter } from '@application/adapters/courseAdapter';
 import { useQuery } from '@tanstack/react-query';
+import { useActiveStudent } from '../coordinators/hooks/useActiveStudent';
 
 export function useCoursesWithLessons() {
   const adapter = useCourseAdapter();
-  const { isAuthenticated } = useAuthAdapter();
+  const { isAuthenticated, isAdmin } = useAuthAdapter();
+  const { appUser, isLoading } = useActiveStudent();
+
   return useQuery({
     queryKey: ['coursesWithLessons'],
-    queryFn: () => adapter.getCoursesWithLessons(),
+    queryFn: () => {
+      // if user is not an admin, or appUser.courseId !== 10, filter out course with id 10
+      if (!isAdmin && appUser?.courseId !== 10) {
+        return adapter
+          .getCoursesWithLessons()
+          .then((courses) => courses.filter((course) => course.id !== 10));
+      }
+      return adapter.getCoursesWithLessons();
+    },
     staleTime: Infinity,
-    enabled: isAuthenticated,
+    enabled: isAuthenticated && !isLoading,
   });
 }
