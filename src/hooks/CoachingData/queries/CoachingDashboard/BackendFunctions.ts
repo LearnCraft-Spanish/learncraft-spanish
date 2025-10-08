ff1d3387e81ea5edc0d60f097b0c73c7b89eecbc
VirtualBlakeWillis <bewillis101@gmail.com>
2025-10-08 12:49:29 -0500

    private calls for coach recent records are sorted by name

--- src/hooks/CoachingData/queries/CoachingDashboard/BackendFunctions.ts
+++ src/hooks/CoachingData/queries/CoachingDashboard/BackendFunctions.ts
@@ -14,14 +14,21 @@ interface RecentRecords {
 export default function useCoachingDashboardBackend() {
   const { getFactory } = useBackendHelpers();
 
-  const getRecentRecords = (
+  const getRecentRecords = async (
     coachName: string | undefined,
     monthYear: string,
   ) => {
     if (!coachName) return undefined;
-    return getFactory<RecentRecords>(
+    const data = await getFactory<RecentRecords>(
       `coaching/recent-records/?coachName=${coachName}&monthYear=${monthYear}`,
     );
+    // sort private calls by student name. we will use the weekName field. a weekName is a string that can look like "Sam Barton Standard: Private-Only Week 23. lets sort by splitting it, grabbing the first name, then comparing"
+    data.privateCalls.sort((a, b) => {
+      const aFirstName = a.weekName.split(' ')[0];
+      const bFirstName = b.weekName.split(' ')[0];
+      return aFirstName.localeCompare(bFirstName);
+    });
+    return data;
   };
 
   return { getRecentRecords };
