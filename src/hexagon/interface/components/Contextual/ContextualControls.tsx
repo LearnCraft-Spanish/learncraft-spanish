529852bb349f705b139517aadf1ca421acb243fc
ClassicalMoser <69487098+ClassicalMoser@users.noreply.github.com>
2025-11-13 15:10:48 -0600

    correct assets directory

--- src/hexagon/interface/components/Contextual/ContextualControls.tsx
+++ src/hexagon/interface/components/Contextual/ContextualControls.tsx
@@ -1,6 +1,6 @@
 import { useContextualMenu } from '@interface/hooks/useContextualMenu';
-import pencil from 'assets/icons/pencil-50.svg';
-import x_dark from 'assets/icons/x_dark.svg';
+import pencil from 'src/assets/icons/pencil-50.svg';
+import x_dark from 'src/assets/icons/x_dark.svg';
 
 // edit function is an optional prop
 export default function ContextualControls({
