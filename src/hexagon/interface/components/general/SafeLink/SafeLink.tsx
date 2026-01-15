52494a3e6a7ffa7a4c2a8bdea873ba8c9e6b7324
VirtualBlakeWillis <bewillis101@gmail.com>
2026-01-15 14:09:37 -0600

    extra documentation

--- src/hexagon/interface/components/general/SafeLink/SafeLink.tsx
+++ src/hexagon/interface/components/general/SafeLink/SafeLink.tsx
@@ -3,6 +3,12 @@ import { Link } from 'react-router-dom';
 import './SafeLink.scss';
 type SafeLinkProps = { disabled?: boolean } & LinkProps;
 
+/**
+ * A wrapper around the Link component that adds a disabled state
+ * This is the right way to handle disabled links
+ *
+ * @param props - The props for the SafeLink component
+ */
 export function SafeLink(props: SafeLinkProps) {
   const { disabled, className, ...rest } = props;
   if (disabled) {
