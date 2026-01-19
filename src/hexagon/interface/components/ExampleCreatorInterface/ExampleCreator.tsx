89ee12200b63853081ea7b3f95298c1f419cb2bf
VirtualBlakeWillis <bewillis101@gmail.com>
2026-01-19 12:17:19 -0600

    using use case hook

--- src/hexagon/interface/components/ExampleCreatorInterface/ExampleCreator.tsx
+++ src/hexagon/interface/components/ExampleCreatorInterface/ExampleCreator.tsx
@@ -1,15 +1,23 @@
-import type { ColumnDisplayConfig } from '@interface/components/EditableTable/types';
+import type { CellRenderProps, ColumnDisplayConfig } from '@interface/components/EditableTable/types';
+import {useExampleCreator} from '@application/useCases/useExampleCreator';
 import {CreateTable} from '@interface/components/CreateTable/CreateTable';
+import { StandardCell } from '@interface/components/EditableTable';
 const exampleDisplayConfig: ColumnDisplayConfig[] = [
-  { id: 'spanish', label: 'spanish', width: '50%' },
-  { id: 'english', label: 'english', width: '50%' },
+  { id: 'spanish', label: 'Spanish', width: '50%' },
+  { id: 'english', label: 'English', width: '50%' },
 ];
 export default function ExampleCreator() {
+  const { tableProps } = useExampleCreator();
+
+  const renderCell = (props: CellRenderProps) => {
+    return <StandardCell {...props} />;
+  };
   return <div>
     <h2>Example Creator</h2>
     <CreateTable
     displayConfig={exampleDisplayConfig}
-   
+    renderCell={renderCell}
+    {...tableProps}
     />
   </div>;
 }
