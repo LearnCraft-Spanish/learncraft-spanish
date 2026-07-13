7af8ad74184cbea2952abce63b8cd26192354c9f
Blake Willis <55206592+VirtualBlakeWillis@users.noreply.github.com>
2026-07-13 10:20:22 -0500

    format

--- src/components/DatabaseTables/VocabQuizDb/ProgramsTable/types/index.ts
+++ src/components/DatabaseTables/VocabQuizDb/ProgramsTable/types/index.ts
@@ -1,12 +1,3 @@
 export type CohortLetter =
-  | 'A'
-  | 'B'
-  | 'C'
-  | 'D'
-  | 'E'
-  | 'F'
-  | 'G'
-  | 'H'
-  | 'I'
-  | 'J';
+  'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J';
 export type CohortField = `cohort${CohortLetter}CurrentLesson`;
