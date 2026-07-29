6f8325dd6ee8ee2638e77efd6ecd881fb19a1f29
Blake Willis <55206592+VirtualBlakeWillis@users.noreply.github.com>
2026-07-29 11:48:10 -0500

    ignore sentry errors I already thought got ignored

--- src/routes/SentryRoutes.ts
+++ src/routes/SentryRoutes.ts
@@ -9,6 +9,7 @@ import {
   useLocation,
   useNavigationType,
 } from 'react-router-dom';
+import { shouldIgnoreError } from './sentryIgnoredErrors';
 
 const SentryRoutes = Sentry.withSentryReactRouterV6Routing(Routes);
 
@@ -32,6 +33,13 @@ Sentry.init({
   ],
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1.0,
+  beforeSend: (event, hint) => {
+    if (shouldIgnoreError(event, hint.originalException)) {
+      return null;
+    }
+
+    return event;
+  },
 });
 
 export default SentryRoutes;
