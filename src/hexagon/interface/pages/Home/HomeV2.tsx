a781c39315cc77e00016923e486a5e136d0d5296
Blake Willis <bewillis101@gmail.com>
2026-09-18 11:47:25 -0500

    better interactive styling for give feedback/report bug"

--- src/hexagon/interface/pages/Home/HomeV2.tsx
+++ src/hexagon/interface/pages/Home/HomeV2.tsx
@@ -52,7 +52,9 @@ export function HomeV2Loaded({ cta, entries }: HomeV2LoadedProps): JSX.Element {
             className={styles.feedbackLink}
             href="mailto:info@learncraftspanish.com"
           >
-            give feedback / report a bug
+            <span className={styles.feedbackLabel}>
+              give feedback / report a bug
+            </span>
             <Icon name="chevronRight" size="inline" tone="inherit" />
           </a>
         </div>
